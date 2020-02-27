dfc2fdd20a9cf5bf75caa2ab76658c93dc099a50
antonin bruneau <antonin.bruneau@gmail.com>
2020-02-27 08:57:13 -0800

    Update transformer to version 3.12.0

---
+++ transformer.rb
@@ -0,0 +1,58 @@
+class Transfromer < Formula
+    desc "DataOps Platform for Modern Data Movement"
+    homepage "https://streamsets.com"
+    url "https://archives.streamsets.com/transformer/3.12.0/tarball/streamsets-transformer-all-3.12.0.tgz"
+    sha256 "66bd1a00027bce4ef29055a6c35d07ecfe99afe41c8d16522c5b8fe40c5602f0"
+
+    bottle :unneeded
+  
+    depends_on :java => "1.8"
+
+    def install
+        prefix.install Dir["*"]
+
+        inreplace "#{libexec}/transformer-env.sh" do |s|
+            s.gsub! "#export TRANSFORMER_LOG=/var/log/transformer", "export TRANSFORMER_LOG=#{var}/log/transformer"
+            s.gsub! "#export TRANSFORMER_DATA=/var/lib/transformer", "export TRANSFORMER_DATA=#{var}/lib/transformer"
+            s.gsub! "#export TRANSFORMER_RESOURCES=/var/lib/transformer-resources", "export TRANSFORMER_RESOURCES=#{var}/lib/transformer-resources"
+            s.gsub! "#export TRANSFORMER_CONF=/etc/transformer", "export TRANSFORMER_CONF=#{etc}/transformer"
+            s << %Q{
+            export TRANSFORMER_DIST=#{opt_prefix}
+            }
+        end
+
+        mkpath "#{var}/lib/transformer-resources"
+
+        (etc/"transformer").install Dir["#{prefix}/etc/*"]
+
+        ohai "For ease of use source #{opt_prefix}/libexec/transformer-env.sh in your .bashrc"
+    end
+
+    plist_options :manual => "streamsets-transformer"
+  
+    def plist; <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>KeepAlive</key>
+          <true/>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/streamsets</string>
+            <string>transformer</string>
+          </array>
+        </dict>
+      </plist>
+      EOS
+    end
+
+    test do
+        system bin/"streamsets", "stagelibs", "-list"
+    end
+
+end
--- update.sh
+++ update.sh
@@ -3,31 +3,50 @@
 export LANG="en_US.UTF-8"
 
 if [ $1 ]; then
-    tag=$1
+    product=$1
+else
+    echo "Please provide a product"
+    exit 0
+fi
+
+if [ $2 ]; then
+    tag=$2
 else
     echo "Please provide a tag"
     exit 0
 fi
 
-wget https://archives.streamsets.com/datacollector/${tag}/tarball/streamsets-datacollector-core-${tag}.tgz
-export sha256=$(shasum -a 256 streamsets-datacollector-core-${tag}.tgz | awk '{print $1}')
-sed -i '' "s/url.*/url \"https\:\/\/archives.streamsets.com\/datacollector\/${tag}\/tarball\/streamsets-datacollector-core-${tag}.tgz\"/" datacollector-core.rb
-sed -i '' "s/sha256.*/sha256 \"${sha256}\"/" datacollector-core.rb
-rm streamsets-datacollector-core-${tag}.tgz
+if [ $product == "sdc" ]; then
 
-wget https://archives.streamsets.com/datacollector/${tag}/tarball/streamsets-datacollector-all-${tag}.tgz
-export sha256=$(shasum -a 256 streamsets-datacollector-all-${tag}.tgz | awk '{print $1}')
-sed -i '' "s/url.*/url \"https\:\/\/archives.streamsets.com\/datacollector\/${tag}\/tarball\/streamsets-datacollector-all-${tag}.tgz\"/" datacollector.rb
-sed -i '' "s/sha256.*/sha256 \"${sha256}\"/" datacollector.rb
-rm streamsets-datacollector-all-${tag}.tgz
+    wget https://archives.streamsets.com/datacollector/${tag}/tarball/streamsets-datacollector-core-${tag}.tgz
+    export sha256=$(shasum -a 256 streamsets-datacollector-core-${tag}.tgz | awk '{print $1}')
+    sed -i '' "s/url.*/url \"https\:\/\/archives.streamsets.com\/datacollector\/${tag}\/tarball\/streamsets-datacollector-core-${tag}.tgz\"/" datacollector-core.rb
+    sed -i '' "s/sha256.*/sha256 \"${sha256}\"/" datacollector-core.rb
+    rm streamsets-datacollector-core-${tag}.tgz
 
-wget https://archives.streamsets.com/datacollector/${tag}/tarball/SDCe/streamsets-datacollector-edge-${tag}-darwin-amd64.tgz
-export sha256=$(shasum -a 256 streamsets-datacollector-edge-${tag}-darwin-amd64.tgz | awk '{print $1}')
-sed -i '' "s/url.*/url \"https\:\/\/archives.streamsets.com\/datacollector\/${tag}\/tarball\/SDCe\/streamsets-datacollector-edge-${tag}-darwin-amd64.tgz\"/" datacollector-edge.rb
-sed -i '' "s/sha256.*/sha256 \"${sha256}\"/" datacollector-edge.rb
-rm streamsets-datacollector-edge-${tag}-darwin-amd64.tgz
+    wget https://archives.streamsets.com/datacollector/${tag}/tarball/streamsets-datacollector-all-${tag}.tgz
+    export sha256=$(shasum -a 256 streamsets-datacollector-all-${tag}.tgz | awk '{print $1}')
+    sed -i '' "s/url.*/url \"https\:\/\/archives.streamsets.com\/datacollector\/${tag}\/tarball\/streamsets-datacollector-all-${tag}.tgz\"/" datacollector.rb
+    sed -i '' "s/sha256.*/sha256 \"${sha256}\"/" datacollector.rb
+    rm streamsets-datacollector-all-${tag}.tgz
+
+    wget https://archives.streamsets.com/datacollector/${tag}/tarball/SDCe/streamsets-datacollector-edge-${tag}-darwin-amd64.tgz
+    export sha256=$(shasum -a 256 streamsets-datacollector-edge-${tag}-darwin-amd64.tgz | awk '{print $1}')
+    sed -i '' "s/url.*/url \"https\:\/\/archives.streamsets.com\/datacollector\/${tag}\/tarball\/SDCe\/streamsets-datacollector-edge-${tag}-darwin-amd64.tgz\"/" datacollector-edge.rb
+    sed -i '' "s/sha256.*/sha256 \"${sha256}\"/" datacollector-edge.rb
+    rm streamsets-datacollector-edge-${tag}-darwin-amd64.tgz
+
+elif [ $product == "transfromer" ]; then
+    wget https://archives.streamsets.com/transformer/${tag}/tarball/streamsets-transformer-all-${tag}.tgz
+    export sha256=$(shasum -a 256 /tarball/streamsets-transformer-all-${tag}.tgz | awk '{print $1}')
+    sed -i '' "s/url.*/url \"https\:\/\/archives.streamsets.com\/transformer\/${tag}\/tarball\/streamsets-transformer-all-${tag}.tgz\"/" transformer.rb
+    sed -i '' "s/sha256.*/sha256 \"${sha256}\"/" transformer.rb
+    rm streamsets-transformer-all-${tag}.tgz
+
+else
+    print "Unknown product"
+fi
 
 git add *
-git commit -m "Update to version ${tag}"
-git tag ${tag}
+git commit -m "Update ${product} to version ${tag}"
 git push
